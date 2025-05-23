275ac7876d2fb933514e3c9e803d4d7121dae836
Matthew <matthewjwright07@gmail.com>
2025-05-23 16:08:13 -0500

    initial commit

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+    srand(time(NULL));
+    int random = (rand()^rand())%100;
+    int guess;
+    while (1){
+        printf("Please enter a number to guess:\n");
+        scanf("%d",&guess);
+        if (guess>random){
+            printf("Too High\n");
+        }else if (guess<random){
+            printf("Too Low\n");
+        }else{
+            printf("You did it\n");
+            break;
+        }
+    }
+    return 0;
+}
